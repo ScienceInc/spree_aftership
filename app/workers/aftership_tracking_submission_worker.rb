a8723252eb2b0905c5d973aec9e82ebc232e4e7a
Bryce Frye <bcfrye19@gmail.com>
2014-09-04 16:26:52 -0700

    removed aftership queue

--- app/workers/aftership_tracking_submission_worker.rb
+++ app/workers/aftership_tracking_submission_worker.rb
@@ -1,6 +1,6 @@
 class AftershipTrackingSubmissionWorker
 	include Sidekiq::Worker if defined?(Sidekiq::Worker)
-	sidekiq_options queue: :aftership, unique: true
+	sidekiq_options unique: true
 
   def perform(id)
     AftershipTracking.find(id).exec_add_to_aftership
