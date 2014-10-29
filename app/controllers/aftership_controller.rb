249793be3459059185be74f37e040f179a986d55
Bryce Frye <bcfrye19@gmail.com>
2014-10-29 12:38:04 -0700

    added testing params for shipment tracking

--- app/controllers/aftership_controller.rb
+++ app/controllers/aftership_controller.rb
@@ -6,7 +6,22 @@ class AftershipController < Spree::BaseController
 				redirect_to root_path
 			}
 			format.json {
-				order = Spree::Order.where("number = ? AND email = ?", params[:order_number], params[:email])
+				# for testing purposes enter order number and email as 'test'
+				if params[:order_number].match(/^test/) && params[:email] = "test"
+					numbers = []
+					num = params[:order_number][-1].to_i
+					if num > 0
+						num.times do |i|
+							numbers << "testtracking#{i+1}"
+						end
+						render json: {success: true, tracking_numbers: numbers} and return
+					else
+						render json: {success: true} and return
+					end
+				else
+					order = Spree::Order.where("number = ? AND email = ?", params[:order_number], params[:email])
+				end
+
 				if order.any?
 					tracked_shipments = order.first.shipments.select{|s| s.tracking.present? }
 					if tracked_shipments.any?
