b21c7830ed349c3bba94d76c9f2266cd538a5bf2
Bryce Frye <bcfrye19@gmail.com>
2015-08-25 18:20:00 -0700

    add new send to aftership method

--- app/models/spree/shipment_decorator.rb
+++ app/models/spree/shipment_decorator.rb
@@ -3,13 +3,30 @@ Spree::Shipment.class_eval do
 
   after_save :send_tracking_to_aftership
 
+  def send_to_aftership
+    if tracking.present?
+      aftership_tracking = aftership_trackings.create(:tracking => tracking, :email => order.email, :order_number => order.number)
+
+      line_items.select{|li| li.tracking.present? && li.aftership_tracking_id.nil?}.each do |line_item|
+        line_item.update_column(:aftership_tracking_id, aftership_tracking.id)
+      end
+
+      aftership_tracking.add_to_aftership
+
+      unless defined?(Delayed::Job) || defined?(Sidekiq::Worker)
+        # If delayed job is not present, have to manually to push all the trackings
+        AftershipTracking.add_to_aftership
+      end
+    end
+  end
+
   private
 
   def send_tracking_to_aftership
     if tracking && tracking_changed?
 
       aftership_tracking = aftership_trackings.create(:tracking => tracking, :email => order.email, :order_number => order.number)
-      
+
       line_items.select{|li| li.tracking.present? && li.aftership_tracking_id.nil?}.each do |line_item|
         line_item.update_column(:aftership_tracking_id, aftership_tracking.id)
       end
